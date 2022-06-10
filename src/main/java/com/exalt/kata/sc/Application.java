1883d1b5850fc56c87becd9fb2c8089e4cb4222e
Zied Guellim <19@zied.dev>
2022-06-10 16:17:45 +0200

    [FEATURE] Step 2

--- src/main/java/com/exalt/kata/sc/Application.java
+++ src/main/java/com/exalt/kata/sc/Application.java
@@ -1,5 +1,6 @@
 package com.exalt.kata.sc;
 
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 import com.exalt.kata.sc.exception.MalformedInputException;
@@ -7,15 +8,14 @@ import com.exalt.kata.sc.exception.MalformedInputException;
 public class Application {
 
     static int add(String numbers) throws MalformedInputException {
-        var pattern = Pattern.compile("(\\s+)|(\\-?\\d+),(\\-?\\d+)");
+        var pattern = Pattern.compile("\\s+|(?:\\-?\\d+,)*\\-?\\d{1}");
         var matcher = pattern.matcher(numbers);
         if (!matcher.matches()) {
             throw new MalformedInputException("Malformed input numbers");
         }
-        return parseInt(matcher.group(2)) + parseInt(matcher.group(3));
-    }
-
-    private static int parseInt(String group) {
-        return group == null ? 0 : Integer.parseInt(group);
+        return Arrays.stream(numbers.split(","))
+                .filter(s -> !s.isBlank())
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
--- src/test/java/com/exalt/kata/sc/ApplicationTest.java
+++ src/test/java/com/exalt/kata/sc/ApplicationTest.java
@@ -1,28 +1,32 @@
 package com.exalt.kata.sc;
 
 import com.exalt.kata.sc.exception.MalformedInputException;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.exalt.kata.sc.Application.*;
 import static org.assertj.core.api.Assertions.*;
-
 
 class ApplicationTest {
 
     @ParameterizedTest
     @CsvSource(textBlock = """
-                 '  ',   0
-                 '1,2',  3
-                 '3,-3', 0
+                 '  ',          0
+                 '1,2',         3
+                 '3,-3',        0
+                 '-1',          -1
+                 '1,2,3',       6
+                 '1,2,3,4',     10
+                 '1,-2,3,4',    6
             """)
     void shouldAddNumbers(String numbers, int result) throws MalformedInputException {
         assertThat(add(numbers)).isEqualTo(result);
     }
 
-    @Test
-    void shouldThrowMalFormedException() {
-        assertThatThrownBy(() -> add("1,2,-3")).isInstanceOf(MalformedInputException.class);
+    @ParameterizedTest
+    @ValueSource(strings = { "1,2,", ",1,2" })
+    void shouldThrowMalFormedException(String numbers) {
+        assertThatThrownBy(() -> add(numbers)).isInstanceOf(MalformedInputException.class);
     }
 }
