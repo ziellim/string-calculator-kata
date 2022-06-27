a45aebaee111829cf8d9cef2ae6d373fb16dfc79
Zied Guellim <19@zied.dev>
2022-06-27 10:52:18 +0200

    [FEATURE] Step 3

--- src/main/java/com/exalt/kata/sc/Application.java
+++ src/main/java/com/exalt/kata/sc/Application.java
@@ -8,12 +8,12 @@ import com.exalt.kata.sc.exception.MalformedInputException;
 public class Application {
 
     static int add(String numbers) throws MalformedInputException {
-        var pattern = Pattern.compile("\\s+|(?:\\-?\\d+,)*\\-?\\d{1}");
+        var pattern = Pattern.compile("\\s+|(?:\\-?\\d+[,\\n])*\\-?\\d{1}");
         var matcher = pattern.matcher(numbers);
         if (!matcher.matches()) {
             throw new MalformedInputException("Malformed input numbers");
         }
-        return Arrays.stream(numbers.split(","))
+        return Arrays.stream(numbers.split("[,\\n]"))
                 .filter(s -> !s.isBlank())
                 .mapToInt(Integer::parseInt)
                 .sum();
--- src/test/java/com/exalt/kata/sc/ApplicationTest.java
+++ src/test/java/com/exalt/kata/sc/ApplicationTest.java
@@ -1,6 +1,7 @@
 package com.exalt.kata.sc;
 
 import com.exalt.kata.sc.exception.MalformedInputException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -24,9 +25,19 @@ class ApplicationTest {
         assertThat(add(numbers)).isEqualTo(result);
     }
 
+    @Test
+    void shouldAddNumberWithLines() throws MalformedInputException {
+        assertThat(add("1,-2\n3,4")).isEqualTo(6);
+    }
+
     @ParameterizedTest
     @ValueSource(strings = { "1,2,", ",1,2" })
     void shouldThrowMalFormedException(String numbers) {
         assertThatThrownBy(() -> add(numbers)).isInstanceOf(MalformedInputException.class);
+    }
+
+    @Test
+    void shouldThrowMalFormedExceptionWithLines() {
+        assertThatThrownBy(() -> add("1,\n")).isInstanceOf(MalformedInputException.class);
     }
 }
